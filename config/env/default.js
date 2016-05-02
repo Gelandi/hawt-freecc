2ff11ad64351063349a68f4a5f1d30c4b471a957
Gelandi <mike.shimp@gmail.com>
2016-05-01 23:37:10 -0700

    Initial commit of barebones MEANjs framework for use in Free code Camp
    Includes a working mongo instance, express native routing,
    	swig templating, sass, and a full feature gulpfile

---
+++ config/env/default.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+  app: {
+    title: 'Taogoat Trail --- Free Code Camp',
+    description: 'ClementineJS framework from Free Code Camp adapted to MEANjs module structure',
+    keywords: 'clementinejs, free code camp, mongodb, express, node.js, mongoose'
+  },
+  port: 3000,
+  templateEngine: 'swig'
+};
---
+++ config/env/development.js
@@ -0,0 +1,62 @@
+'use strict';
+
+var defaultEnvConfig = require('./default');
+
+module.exports = {
+  db: {
+    uri: 'mongodb://gelandi:#Freeccc2016@ds021741.mlab.com:21741/freecc-mean',
+    options: {
+      user: '',
+      pass: ''
+    },
+    // Enable mongoose debug mode
+    debug: process.env.MONGODB_DEBUG || false
+  },
+  log: {
+    // logging with Morgan - https://github.com/expressjs/morgan
+    // Can specify one of 'combined', 'common', 'dev', 'short', 'tiny'
+    format: 'dev',
+    options: {
+      // Stream defaults to process.stdout
+      // Uncomment/comment to toggle the logging to a log on the file system
+      //stream: {
+      //  directoryPath: process.cwd(),
+      //  fileName: 'access.log',
+      //  rotatingLogs: { // for more info on rotating logs - https://github.com/holidayextras/file-stream-rotator#usage
+      //    active: false, // activate to use rotating logs 
+      //    fileName: 'access-%DATE%.log', // if rotating logs are active, this fileName setting will be used
+      //    frequency: 'daily',
+      //    verbose: false
+      //  }
+      //}
+    }
+  },
+  app: {
+    title: defaultEnvConfig.app.title + ' - Development Environment'
+  },
+  livereload: true,
+  seedDB: {
+    seed: process.env.MONGO_SEED === 'true' ? true : false,
+    options: {
+      logResults: process.env.MONGO_SEED_LOG_RESULTS === 'false' ? false : true,
+      seedUser: {
+        username: process.env.MONGO_SEED_USER_USERNAME || 'user',
+        provider: 'local',
+        email: process.env.MONGO_SEED_USER_EMAIL || 'user@localhost.com',
+        firstName: 'User',
+        lastName: 'Local',
+        displayName: 'User Local',
+        roles: ['user']
+      },
+      seedAdmin: {
+        username: process.env.MONGO_SEED_ADMIN_USERNAME || 'admin',
+        provider: 'local',
+        email: process.env.MONGO_SEED_ADMIN_EMAIL || 'admin@localhost.com',
+        firstName: 'Admin',
+        lastName: 'Local',
+        displayName: 'Admin Local',
+        roles: ['user', 'admin']
+      }
+    }
+  }
+};
